7eee406dc64966baa67bed60f1ba4c8e5f2934bb
Ningen0205 <57162567+Ningen0205@users.noreply.github.com>
2021-10-26 15:10:10 +0900

    関数に分割

--- src/scrape/main.ts
+++ src/scrape/main.ts
@@ -1,41 +1,55 @@
-import puppeteer from "puppeteer"
+import puppeteer from 'puppeteer';
 
-const run = async () => {
-    const browser = await puppeteer.launch({
-        slowMo: 100
-    })
-    const page = await browser.newPage()
+const getUpdatedTime = async (section: puppeteer.ElementHandle) => {
+    const updatedTime = await section.$eval('.title-block-note', (e) => e.textContent);
+    if (updatedTime === null) {
+        throw new Error('更新日時情報が見つかりませんでした。');
+    }
 
-    await page.goto('https://mainichi.jp/covid19',{
-        waitUntil: 'networkidle2'
-    })
+    const replacedUpdatedTime = updatedTime.replace('時点', '').replace('月', '/').replace('日', '');
+    const dateUpdatedTime = new Date(replacedUpdatedTime);
+    return dateUpdatedTime;
+};
 
-    const section = await page.$('#c-pref')
-    if(section === null) throw new Error('セクション要素が見つかりませんでした。')
+const getSection = async (page: puppeteer.Page) => {
+    const section = await page.$('#c-pref');
+    if (section === null) {
+        throw new Error('セクション要素が見つかりませんでした。');
+    }
 
-    const updateTime = await section.$('.title-block-note')
-    if(updateTime === null) throw new Error('更新日時の要素が見つかりませんでした。')
+    return section;
+};
 
-    const updateTimeHandle = await updateTime.getProperty('textContent') as puppeteer.ElementHandle
-
-    const updateTimeContext: string = await updateTimeHandle.jsonValue()
-    
-    console.log(new Date(updateTimeContext.replace('時点', '').replace('月', '/').replace('日', '')))
-
+const getTodayJapanCovid19InfectionResult = async (updatedTime: Date, page: puppeteer.Page) => {
     const result: todayJapanCovid19InfectionResult = {
-        updateTime: new Date(updateTimeContext.replace('時点', '').replace('月', '/').replace('日', '')),
-        todayInfection: {}
+        updateTime: new Date(),
+        todayInfection: {},
+    };
+
+    for (let i = 1; i < 48; i++) {
+        const elmHandle = await (await page.$(`#mc${i}`))?.getProperty('textContent');
+        const [, prefecture, todayInfection] = ((await elmHandle?.jsonValue()) as string).split(' ');
+        result.todayInfection[prefecture] = Number(todayInfection);
     }
 
-    for(let i=1; i<48; i++) {
-        const elmHandle = await (await page.$(`#mc${i}`))?.getProperty('textContent')
-        const [, prefecture, todayInfection ] = (await elmHandle?.jsonValue() as string).split(' ')
-        result.todayInfection[prefecture] = Number(todayInfection)
-    }
+    return result;
+};
 
-    console.log(result)
+const run = async (): Promise<todayJapanCovid19InfectionResult> => {
+    const browser = await puppeteer.launch({
+        slowMo: 100,
+    });
+    const page = await browser.newPage();
 
-    await browser.close()
-}
+    await page.goto('https://mainichi.jp/covid19', {
+        waitUntil: 'networkidle2',
+    });
 
-run()
+    const section = await getSection(page);
+    const updatedTime = await getUpdatedTime(section);
+    const scrapeResult: todayJapanCovid19InfectionResult = await getTodayJapanCovid19InfectionResult(updatedTime, page);
+
+    await browser.close();
+};
+
+run();
